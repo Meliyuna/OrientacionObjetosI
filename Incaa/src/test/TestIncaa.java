2e014c0cea0068d57112cee534f37fee7bac71ab
Melisa Romero <melisa.e.romero@gmail.com>
2016-09-20 22:33:41 -0300

    cambio de nombre

---
+++ Incaa/src/test/TestIncaa.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import modelo.Incaa;
+import modelo.Genero;
+import modelo.Pelicula;
+
+
+public class TestIncaa {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
